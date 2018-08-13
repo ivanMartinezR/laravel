9ce227b3078366eaa9a5c747c4c9db3ab0a36fa9
unknown <IvanMartinezR>
2018-08-13 10:47:45 -0500

    'primero'

---
+++ app/Http/Controllers/MessagesController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Message;
+use App\Http\Requests\CreateMessageRequest;
+use Illuminate\Http\Request;
+
+class MessagesController extends Controller
+{
+	//el parametro se crea solo en realidad tu mandas el id
+    public function show(Message $message){
+
+    	return View('messages.show',[
+    		'message'=>$message,
+    	]);
+    }
+
+    public function create(CreateMessageRequest $request){
+
+    	$message=Message::create([
+    		'content'=>$request->input('message'),
+    		'image'=>'http://lorempixel.com/600/338?'.mt_rand(0,1000)
+    	]);
+
+    	//dd($message);
+
+    	return redirect('/messages/'.$message->id);
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="jumbotron text-center">
+	<h1>Laratter</h1>
+	<nav>
+		<ul class="nav nav-pills">
+			<li class="nav-item">
+				<a class="nav-link" href="/">Home</a>
+			</li>
+		</ul>
+	</nav>
+</div>
+<div class="row" >
+	<form action="/messages/create" method="post">
+		<div class="form-group @if($errors->has('message')) has-danger @endif">
+			{{csrf_field()}}
+			<input type="text" name="message" class="form-control" placeholder="¿Qué estás pensando?" />	
+			@if($errors->has('message'))
+				@foreach($errors->get('message') as $error)
+					<div class="form-control-feedback"> {{$error}} </div>
+				@endforeach
+			@endif		
+		</div>
+	</form>
+</div>
+<div class="row">
+	@forelse($messages as $message)
+		<div class="col-6">
+			<img class="img-thumbnail" src="{{ $message->image }}">
+			<p class="card-text">
+				{{ $message->content }}
+				<a href="/messages/{{ $message->id }}">Leer más</a>
+			</p>
+		</div>
+	@empty
+		<p>No hay mensajes destacados.</p>
+	@endforelse
+</div>
+@endsection
